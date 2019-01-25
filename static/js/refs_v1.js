9741aab9fbd53d773f3d16f66cf7a28f55b8749b
Fabian Pedregosa <pedregosa@google.com>
2019-01-25 11:04:36 -0500

    custom favicon location

--- static/js/refs_v1.js
+++ static/js/refs_v1.js
@@ -60,10 +60,11 @@ function doTOC() {
     var template = "<p class='framed'><b>Outline:</b><br/>";
     var sections = document.querySelectorAll("h2");
     sections.forEach(function(element, i){
-        var text = element.innerHTML
-        var sectTitle = `${i+1}. ${text}`
-        element.innerHTML = sectTitle
-        template = template + `<span style="margin-left: 20px"><a href="#">${sectTitle}</a></span><br/>`
+        var text = element.innerHTML;
+        var sectTitle = `${i+1}. ${text}`;
+        element.innerHTML = sectTitle;
+        element.setAttribute('id', 'sec' + (i+1));
+        template = template + `<span style="margin-left: 20px"><a href="#sec${i+1}">${sectTitle}</a></span><br/>`;
     })
 
     var TOC = document.getElementById("TOC")
